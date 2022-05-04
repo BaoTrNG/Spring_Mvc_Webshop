ed22ed555085504649b51b9c50a4f0f61de8d162
brjdyxter <brjdyxter@gmail.com>
2022-05-04 17:08:20 +0700

    d

---
+++ src/main/java/ptithcm/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package ptithcm.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping()
+	public String welcome() {
+		return"welcome";
+	}
+	
+}
